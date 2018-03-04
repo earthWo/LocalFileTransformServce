1abf3430ae72e13dd61dd171c6fb744cbf95f9c8
wuzefeng <976397068@qq.com>
2018-03-04 17:40:32 +0800

    将心跳、文本、文件分开，优化传输，便面大文件堵塞

--- SocketConnect/src/socketconnect/con/SocketHelper.java
+++ SocketConnect/src/socketconnect/con/SocketHelper.java
@@ -35,21 +35,32 @@ public class SocketHelper {
 
     private SocketCallback mSocketCallback;
 
-    private ConnectThread mConnectThread;
-    
-    private ServerSocket servce;
-    
+    private MessageThread mConnectMessageThread;
+
+    private HeartThread mConnectHeartThread;
+
+    private ServerSocket messageServce;
+
+    private FileThread mFileThread;
+
+    private ServerSocket heartServce;
+
+    private ServerSocket fileServce;
+
     private static final int PORT=20006;
-    
-    
+
+    private static final int HEART_PORT=20007;
+
+    private static final int FILE_PORT=20008;
+
     private Map<Integer,Connecter>mConnecter;
 
 
     private SocketHelper(){
         mConnecter=new HashMap<>();
     }
-    
-    
+
+
 
 
     public static SocketHelper getInstance(){
@@ -62,8 +73,8 @@ public class SocketHelper {
         }
         return sInstance;
     }
-    
-    
+
+
     public void disconncted(Connecter connceter) {
         if (mSocketCallback != null) {
             mSocketCallback.disconnected(connceter);
@@ -76,11 +87,25 @@ public class SocketHelper {
      * 连接socket
      */
     public void connect(){
-        if(mConnectThread==null){
-            mConnectThread=new ConnectThread();
-            mConnectThread.setName("连接线程");
-            mConnectThread.start();
+        if(mConnectHeartThread==null){
+            mConnectHeartThread=new HeartThread();
+            mConnectHeartThread.setName("心跳线程");
+            mConnectHeartThread.start();
         }
+        if(mConnectMessageThread==null){
+            mConnectMessageThread=new MessageThread();
+            mConnectMessageThread.setName("消息线程");
+            mConnectMessageThread.start();
+        }
+        if(mFileThread==null){
+            mFileThread=new FileThread();
+            mFileThread.setName("文件线程");
+            mFileThread.start();
+        }
+    }
+
+    public Connecter getConnecter(int key){
+        return mConnecter.get(key);
     }
 
 
@@ -89,25 +114,63 @@ public class SocketHelper {
     }
 
 
-    private void connectSuccess(Socket socket){
-        Connecter connecter=new Connecter(socket);
+    private void connectMessageSuccess(Socket messageSocket){
+        Connecter connecter;
+        int key=messageSocket.getInetAddress().hashCode();
+        if(mConnecter.containsKey(key)){
+            connecter=mConnecter.get(key);
+        }else{
+            connecter=new Connecter();
+            mConnecter.put(key,connecter);
+            connecter.setSocketId(key);
+        }
+        connecter.setMessageSocket(messageSocket);
         SocketMessageSender.createMessageSender(connecter);
         SocketMessageReceiver.createMessageReceiver(connecter);
-        SocketHeartSender.createHeardMessageSender(connecter);
-        mConnecter.put(connecter.getSocketId(),connecter);
+    }
+
+    private void connectHeartSuccess(Socket heartSocket){
+        Connecter connecter;
+        int key=heartSocket.getInetAddress().hashCode();
+        if(mConnecter.containsKey(key)){
+            connecter=mConnecter.get(key);
+        }else{
+            connecter=new Connecter();
+            mConnecter.put(key,connecter);
+            connecter.setSocketId(key);
+        }
+        connecter.setHeartSocket(heartSocket);
+        SocketHeartSender.getInstance();
+        SocketHeartReceiver.createMessageReceiver(connecter);
         if(mSocketCallback!=null){
             mSocketCallback.connected(connecter);
         }
-        
     }
-    
+
+    private void connectFileSuccess(Socket fileSocket){
+        Connecter connecter;
+        int key=fileSocket.getInetAddress().hashCode();
+        if(mConnecter.containsKey(key)){
+            connecter=mConnecter.get(key);
+        }else{
+            connecter=new Connecter();
+            mConnecter.put(key,connecter);
+            connecter.setSocketId(key);
+        }
+        connecter.setFileSocket(fileSocket);
+        SocketFileSender.createFileSender(connecter);
+        SocketFileReceiver.createFileReceiver(connecter);
+    }
+
+
+
     public void receiverMessage(Connecter connecter,SocketMessage message){
         if (this.mSocketCallback != null) {
             this.mSocketCallback.receiveMessage(message,connecter);
-        } 
+        }
     }
-    
-    
+
+
     public void sendMessageError(SocketMessage message,SocketException e){
         if(message!=null){
             MessageCallback callback=CallbackSet.get().getCallback(message.getMessageId());
@@ -115,7 +178,7 @@ public class SocketHelper {
                 callback.requestError(message, e);
             }
         }
-        
+
     }
 
 
@@ -124,7 +187,7 @@ public class SocketHelper {
             mSocketCallback.connectError(new SocketException(e.getMessage()));
         }
     }
-    
+
     private void serviceStart() {
         if (mSocketCallback != null) {
             try {
@@ -138,24 +201,24 @@ public class SocketHelper {
 
     public void closeAllConnect(){
         try {
-            mConnectThread=null;
-            SocketHeartSender.closeThreads();
+            mConnectMessageThread=null;
+            SocketHeartSender.getInstance().closeThread();
             SocketMessageSender.closeThreads();
             SocketMessageReceiver.closeThreads();
             mConnecter.clear();
-            servce.close();
-            servce=null;
+            messageServce.close();
+            messageServce=null;
         } catch (IOException ex) {
             Logger.getLogger(SocketHelper.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     public void closeConnect(Connecter connecter) {
-       SocketHeartSender.closeThread(connecter);
-       SocketMessageSender.closeThread(connecter);
-       SocketMessageReceiver.closeThread(connecter);
-       connecter.closeSocket();
-       mConnecter.remove(connecter.getSocketId());
+        SocketHeartSender.getInstance().closeThread();
+        SocketMessageSender.closeThread(connecter);
+        SocketMessageReceiver.closeThread(connecter);
+        connecter.closeConnect();
+        mConnecter.remove(connecter.getSocketId());
     }
 
 
@@ -170,7 +233,7 @@ public class SocketHelper {
             SocketMessageSender.get(socketId).addMessage(socketMessage);
         }
     }
-    
+
     public void sendVoiceMessage(int socketId,int messageId, String fileName) {
         if (socketId!=0) {
             SocketVoiceMessage socketMessage = new SocketVoiceMessage();
@@ -186,7 +249,7 @@ public class SocketHelper {
                 }
             }
             socketMessage.setText(fileName.substring(fileName.lastIndexOf("/")+1));
-            SocketMessageSender.get(socketId).addMessage(socketMessage);
+            SocketFileSender.get(socketId).addMessage(socketMessage);
         }
     }
 
@@ -205,7 +268,7 @@ public class SocketHelper {
                 }
             }
             socketMessage.setText(fileName.substring(fileName.lastIndexOf("/")+1));
-            SocketMessageSender.get(socketId).addMessage(socketMessage);
+            SocketFileSender.get(socketId).addMessage(socketMessage);
         }
     }
 
@@ -224,13 +287,13 @@ public class SocketHelper {
                 }
             }
             socketMessage.setText(fileName.substring(fileName.lastIndexOf("/")+1));
-            SocketMessageSender.get(socketId).addMessage(socketMessage);
+            SocketFileSender.get(socketId).addMessage(socketMessage);
         }
     }
 
     public void sendFileMessage(int socketId,int messageId, String fileName) {
         if (socketId != 0) {
-            
+
             SocketFileMessage socketMessage = new SocketFileMessage();
             socketMessage.setMessageId(messageId);
             try {
@@ -244,12 +307,12 @@ public class SocketHelper {
                 }
             }
             socketMessage.setText(fileName.substring(fileName.lastIndexOf("/")+1));
-            SocketMessageSender.get(socketId).addMessage(socketMessage);
+            SocketFileSender.get(socketId).addMessage(socketMessage);
         }
     }
 
-    
-    
+
+
     public void sendHeardMessage(int socketId,int messageId) {
         if (socketId != 0) {
             SocketTextMessage socketMessage = new SocketTextMessage();
@@ -257,32 +320,83 @@ public class SocketHelper {
             socketMessage.setData("这是一个心跳".getBytes());
             socketMessage.setMessageType(MessageType.MH);
             socketMessage.setText("这是一个心跳");
-            SocketMessageSender.get(socketId).addMessage(socketMessage);
+
+            SocketHeartSender.SocketHeartMessage message=new SocketHeartSender.SocketHeartMessage();
+            Connecter connecter=SocketHelper.getInstance().getConnecter(socketId);
+            if(connecter!=null&&connecter.getHeartSocket()!=null){
+                message.setSocket(connecter.getHeartSocket());
+                message.setMessageType(MessageType.MH);
+                message.setSocketMessage(socketMessage);
+            }
+            SocketHeartSender.getInstance().addMessage(message);
         }
     }
 
 
-   
 
-    private class ConnectThread extends Thread{
+
+    private class MessageThread extends Thread{
 
         @Override
         public void run() {
             //未初始化或未连接
-            if(servce==null){
+            if(messageServce==null){
                 try {
-                    servce=new ServerSocket(PORT);
+                    messageServce=new ServerSocket(PORT);
                     serviceStart();
                     while(true){
-                       Socket s=servce.accept();
-                       connectSuccess(s);
+                        Socket s=messageServce.accept();
+                        connectMessageSuccess(s);
                     }
-                                      
+
                 } catch (IOException ex) {
-                     connectFailure(ex);
+                    connectFailure(ex);
                 }
             }
-           
+
+        }
+    }
+
+    private class HeartThread extends Thread{
+
+        @Override
+        public void run() {
+            //未初始化或未连接
+            if(heartServce==null){
+                try {
+                    heartServce=new ServerSocket(HEART_PORT);
+                    serviceStart();
+                    while(true){
+                        Socket s=heartServce.accept();
+                        connectHeartSuccess(s);
+                    }
+
+                } catch (IOException ex) {
+                    connectFailure(ex);
+                }
+            }
+
+        }
+    }
+
+    private class FileThread extends Thread{
+
+        @Override
+        public void run() {
+            //未初始化或未连接
+            if(fileServce==null){
+                try {
+                    fileServce=new ServerSocket(FILE_PORT);
+                    while(true){
+                        Socket s=fileServce.accept();
+                        connectFileSuccess(s);
+                    }
+
+                } catch (IOException ex) {
+                    connectFailure(ex);
+                }
+            }
+
         }
     }
 
